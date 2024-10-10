f3e23b6a4ae1347cbed311d2f7d48da931e85c32
jamiekimjavier <jamiekimjavier@gmail.com>
2024-10-10 10:07:53 +0800

    Add files via upload

---
+++ Name-Greeter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+    
+    printf("Hello, %s. Nice to meet you! \n", name);
+
+
+    return 0;
+}
